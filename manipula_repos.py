a94e0a685e3d9712f918d079bdee5e8f13392424
ruansoarespy <ruanferreirasoares3@gmail.com>
2024-12-07 17:23:45 -0300

    manipula_repos.py

--- manipula_repos.py
+++ manipula_repos.py
@@ -6,7 +6,7 @@ class ManipulaRepositorios:
     def __init__(self, username):
         self.username = username
         self.api_base_url = 'https://api.github.com'
-        self.access_token = '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'
+        self.access_token = 'seu token'
         self.headers = {
             'Authorization': "Bearer " + self.access_token,
             'X-GitHub-Api-Version': '2022-11-28'
